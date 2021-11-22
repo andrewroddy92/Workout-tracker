5a10055bc34862a34f8b24682ba5a6bbfeaf92dc
andrewroddy92 <andrewroddy92@yahoo.com>
2021-11-21 21:59:33 -0500

    deployed to heroku

--- server.js
+++ server.js
@@ -30,7 +30,12 @@ db.on("error", error => {
     console.log("Database Error:", error);
 });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts",   {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 //-------------- ROUTE ---------------
 
